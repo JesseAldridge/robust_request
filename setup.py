5c544ff31d86d49fed19cf422dedabb0a6a95d33
Jesse Aldridge <jessealdridge@gmail.com>
2021-03-07 21:48:36 -0600

    pip installable

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.0.1'
+
+setup(name='robust_request',
+      version=version,
+      description="A more reliable http request.",
+      long_description="""""",
+      classifiers=[],
+      keywords='http requests',
+      author='Jesse Aldridge',
+      author_email='JesseAldridge@gmail.com',
+      url='https://github.com/JesseAldridge/robust_request',
+      license='MIT',
+      packages=['robust_request'],
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ]
+      )
