47051886b176d9755b022454ac21ac3457c77734
Jesse Aldridge <jessealdridge@gmail.com>
2021-06-06 23:44:18 -0500

    robust get

--- robust_request/requester.py
+++ robust_request/requester.py
@@ -5,12 +5,13 @@ import requests
 
 class Requester:
   def __init__(self, logger):
+    self.session = requests.session()
     self.logger = logger
     self.failure_count = 0
 
-  def json(self, url, session):
+  def json(self, url):
     try:
-      return session.get(url).json()
+      return self.get(url).json()
     except json.decoder.JSONDecodeError:
       return None
     except:
@@ -19,11 +20,31 @@ class Requester:
         self.logger.error('failed too many times, giving up')
         raise
 
+  def get(self, *a, **kw):
+    for attempt_count in range(5):
+      try:
+        return self.session.get(*a, **kw)
+      except(
+        requests.exceptions.ChunkedEncodingError,
+        requests.exceptions.ConnectionError,
+        requests.exceptions.ConnectTimeout,
+        requests.exceptions.ReadTimeout,
+        requests.exceptions.TooManyRedirects,
+        requests.exceptions.SSLError,
+      ) as e:
+        self.logger.error("e: ", e)
+        self.logger.error("type: ", type(e))
+        self.logger.error("args: ", e.args)
+        if hasattr(e, 'errno'):
+          self.logger.error("Index error({0}): {1}".format(e.errno, e.strerror))
+        self.logger.debug(f'connection error, trying again, attempt_count: {attempt_count}')
+        time.sleep(attempt_count)
+
+
 def test():
   url = 'https://jsonplaceholder.typicode.com/todos/1'
-  session = requests.session()
   requester = Requester(logger=logging.getLogger())
-  resp_dict = requester.json(url, session)
+  resp_dict = requester.json(url)
   print('resp_dict:', resp_dict)
 
 if __name__ == '__main__':
